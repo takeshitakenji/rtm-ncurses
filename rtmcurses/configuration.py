771ac691937c707b385b7d3eb9d8d15de853c5b5
Neil E. Hodges <neil@superflex.neilnet>
2012-01-30 06:56:46 -0800

    Setting configuration variables from a dict.

--- rtmcurses/configuration.py
+++ rtmcurses/configuration.py
@@ -3,9 +3,44 @@
 # Redistribution of this file is permitted under
 # the terms of the BSD license
 
-history_size=20
-max_view_height=200
+from os.path import join, isfile
+from os import environ
+from sys import stderr
+from re import compile
 
-rtm_api_key = ""
-rtm_shared_secret = ""
-rtm_token = ""
+supported_vars = dict(
+  history_size=20,
+  max_view_height=200,
+
+  rtm_api_key = "",
+  rtm_shared_secret = "",
+  rtm_token = "",
+)
+
+if 'HOME' in environ:
+  HOME = environ['HOME']
+elif 'USERPROFILE' in environ:
+  HOME = environ['USERPROFILE']
+else:
+  print >>stderr, 'Unable to determine home directory; setting defaults'
+
+configuration_file = join(HOME, '.rtmcurses.conf')
+conf_regex = compile(r'^(\S+)\s*=\s*(.*?)$')
+if isfile(configuration_file):
+  with open(configuration_file, 'rU') as f:
+    for line in f:
+      line = line.rstrip()
+      if not line.startswith('#'):
+        m = conf_regex.search(line)
+        if m:
+          if m.group(1) in supported_vars:
+            value = int(m.group(2)) if m.group(2).isdigit() else value
+            supported_vars[m.group(1)] = value
+        else:
+          print >>stderr, 'Invalid syntax:', line
+else:
+  print >>stderr, 'No such file: %s Setting defaults' % configuration_file
+
+
+for key, value in supported_vars.iteritems():
+  setattr(__builtins__, key, value)
