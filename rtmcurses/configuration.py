12762f1ad980ddd627d9424cc397049ab3ff242a
Neil E. Hodges <neil@superflex.neilnet>
2012-01-30 07:38:18 -0800

    Switched to using ConfigParser to read in $HOME/.config/rtmcurses.conf.

--- rtmcurses/configuration.py
+++ rtmcurses/configuration.py
@@ -7,21 +7,17 @@ from os.path import join, isfile
 from os import environ
 from sys import stderr
 from re import compile
+from ConfigParser import ConfigParser, RawConfigParser
 
-supported_vars = frozenset([
-  'history_size',
-  'max_view_height',
-  'rtm_api_key',
-  'rtm_shared_secret',
-  'rtm_token',
-])
+configuration_parser = ConfigParser(allow_no_value = True)
+configuration_parser.add_section('Display')
+configuration_parser.set('Display', 'history_size', '20')
+configuration_parser.set('Display', 'max_view_height', '480')
 
-history_size=20
-max_view_height=200
-
-rtm_api_key = None
-rtm_shared_secret = None
-rtm_token = None
+configuration_parser.add_section('Keys')
+configuration_parser.set('Keys', 'rtm_api_key', None)
+configuration_parser.set('Keys', 'rtm_shared_secret', None)
+configuration_parser.set('Keys', 'rtm_token', None)
 
 if 'HOME' in environ:
   HOME = environ['HOME']
@@ -30,23 +26,16 @@ elif 'USERPROFILE' in environ:
 else:
   print >>stderr, 'Unable to determine home directory; setting defaults'
 
-configuration_file = join(HOME, '.rtmcurses.conf')
+configuration_file = join(HOME, '.config', 'rtmcurses.conf')
 conf_regex = compile(r'^\s*(\S+)\s*=\s*(.*?)$')
 if isfile(configuration_file):
   with open(configuration_file, 'rU') as f:
-    for line in f:
-      line = line.rstrip()
-      if line and not line.startswith('#'):
-        m = conf_regex.search(line)
-        if m:
-          if m.group(1) in supported_vars:
-            value = m.group(2)
-            if not value:
-              value = None
-            elif value.isdigit():
-              value = int(value)
-            globals()[m.group(1)] = value
-        else:
-          print >>stderr, 'Invalid syntax:', line
+    configuration_parser.read(f)
 else:
   print >>stderr, 'No such file: %s Setting defaults' % configuration_file
+
+
+for key, value in configuration_parser.items('Display'):
+  globals()[key] = int(value)
+for key, value in configuration_parser.items('Keys'):
+  globals()[key] = value
